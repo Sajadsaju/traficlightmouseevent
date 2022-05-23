e3a6fd561123610f066125b67b2e996e5867f9f2
Sajadsaju <sajadsalam99@gmail.com>
2022-05-23 22:08:50 +0530

    first commit

---
+++ trafic.js
@@ -0,0 +1,20 @@
+function fun() {
+
+	document.getElementById("red").style.backgroundColor ="red";
+	document.getElementById("yellow").style.backgroundColor ="black";
+	document.getElementById("green").style.backgroundColor ="black";
+}
+
+		function one(){
+	document.getElementById("red").style.backgroundColor="black";
+	document.getElementById("yellow").style.backgroundColor="#FF9900";
+	document.getElementById("green").style.backgroundColor="black";
+}
+		function two(){
+	document.getElementById("red").style.backgroundColor="black";
+	document.getElementById("yellow").style.backgroundColor="black";
+	document.getElementById("green").style.backgroundColor="green";
+}
+
+
+
